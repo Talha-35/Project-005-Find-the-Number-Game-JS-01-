f2e6a6e6d278120b24eb712c2588b6ef6d008da7
Talha-35 <67706755+Talha-35@users.noreply.github.com>
2020-09-23 16:05:44 +0200

    some files are uploaded

--- Guess_Number.js
+++ Guess_Number.js
@@ -15,15 +15,22 @@ function check() {
         document.getElementById("Res").innerHTML = "Heyyyy Mannnnn... Please write a number between 1-100 !!!" 
     } else if (firstNumber === parseInt(num_1)) {
         document.getElementById("Res").innerHTML = "Heyyyy Mannnnn...You did a great job. congratulations you are the Winner." 
-        count = 1;
+        alert(`You are the Winner:D. You guest after ${count-1}`);  
+        alert("Do you wanna play again!");
+        document.getElementById('inp_1').value=""; 
+        firstNumber= Math.floor((Math.random() * 100) + 1);  
+        count = 1     
     } else if (firstNumber > parseInt(num_1)) {
-        document.getElementById("Res").innerHTML = "OPPPSSSS...you should try again. I think a higher  number"   
+        document.getElementById("Res").innerHTML = "OPPPSSSS...you should try again. I think a higher  number"          
+        document.getElementById("Res_2").innerHTML = `You have tried ${count} times..`;
+        
         count += 1
     } else  {
             document.getElementById("Res").innerHTML = "OPPPSSSS...you should try again. I think a lower number" 
+            document.getElementById("Res_2").innerHTML = `You have tried ${count} times..`;
             count += 1;
         }
 }
 
 
-console.log("object");
+// console.log("object");
